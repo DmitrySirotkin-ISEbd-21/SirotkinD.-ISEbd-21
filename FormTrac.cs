1ccfb3e72336f9872f31f86ad5697babdfa5ad4d
Dmitry <63369896+DmitrySirotkin-ISEbd-21@users.noreply.github.com>
2020-04-10 09:38:14 +0300

    Add files via upload

---
+++ FormTrac.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsTrac
+{
+    public partial class FormTract : Form
+    {
+        private BigTrac tractor;
+        public FormTract()
+        {
+            InitializeComponent();
+        }
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxTrac.Width, pictureBoxTrac.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            tractor.Drawtractor(gr);
+            pictureBoxTrac.Image = bmp;
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            tractor = new BigTrac(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Green);
+            tractor.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxTrac.Width,pictureBoxTrac.Height);
+            Draw();
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    tractor.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    tractor.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    tractor.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    tractor.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+        private void buttonCreate1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            tractor = new Trac(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Green,Color.Gray, true, true);
+            tractor.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxTrac.Width,pictureBoxTrac.Height);
+            Draw();
+        }
+    }
+}
